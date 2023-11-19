4e1dd59f68453264f93f2d12d313d09b405bcff9
ddddfsfe <148766544+ddddfsfe@users.noreply.github.com>
2023-11-20 01:48:57 +0800

    Add files via upload

---
+++ 0-0-3.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{input A;
+input B;
+ input C;
+ Add A B D;
+ Add C D D;
+ Output D;
+}
---
+++ 0-0-5.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{int a=72,b=87,c=93;
+double k;
+k=(a+b+c)/3;
+printf("%f,k");
+return 0;
+ } 
